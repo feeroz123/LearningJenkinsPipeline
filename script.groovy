dcdbd30b9561d4157aa6e38c81800716d2effea9
Feeroz Alam <feeroz123@gmail.com>
2022-04-18 00:11:11 +0530

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,13 @@
+def buildApp() {
+  echo "This is the building method from the groovy file" 
+}
+
+def testApp() {
+  echo "This is the testing method from the groovy file" 
+}
+
+def deployApp() {
+  echo "This is the deployment method from the groovy file" 
+}
+
+return this
